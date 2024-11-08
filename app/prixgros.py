8c7808f9874a44ee6eef8a49a54896afac1ebc39
Antoine Moix <antoine.moix@gmail.com>
2024-11-08 17:12:27 +0100

    added guess

--- app/prixgros.py
+++ app/prixgros.py
@@ -1,6 +1,7 @@
 import datetime
-
-from flask import Flask
+import orjson
+from dataclasses import dataclass
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,17 +9,33 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     game = load_today_game()
-    return "<p>Hello, World!</p>"
+    return game.json()
 
 
 def load_today_game():
-    game = Game(datetime.datetime.now().date, "Aproz Cristal", None, None, 3.50)
+    game = Game(datetime.datetime.now().date(), "Aproz Cristal", None, None, 3.50)
     return game
 
 
+@app.route("/guess", methods=["POST"])
+def submit_guess():
+    return request.json
+
+
+@dataclass
 class Game:
-    date = None
-    item_name = None
-    migros_link = None
-    migros_image = None
-    price = None
+    date: datetime
+    name: str
+    article_link: str
+    image_link: str
+    price: float
+
+    def __init__(self, date, name, article_link, image_link, price) -> None:
+        self.date = date
+        self.name = name
+        self.article_link = article_link
+        self.image_link = image_link
+        self.price = price
+
+    def json(self):
+        return orjson.dumps(self)
